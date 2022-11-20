82f8a558c78bbb2989288bebfd3770f80fd405b4
vishal <vishalpopat8@gmail.com>
2022-11-20 20:57:12 +0530

    commit

---
+++ cmd.js
@@ -0,0 +1,10 @@
+// # 1 - Install Node js -> https://nodejs.org/en/download/
+// # 2 Setup the env for node js ->  youtube
+// # 3 - Create react app -> npx create-react-app [name of app]
+// # 4 - Run the app -> npm start
+// # 5 - Install axios -> npm install axios
+
+
+// ? How to style a react component
+// # Create a css file and import it in the component, and then use the class name in the component
+// # use style attribute to style the component
---
+++ src/components/content/Content.js
@@ -0,0 +1,18 @@
+import './Content.css';
+import RowData from '../../constants/URLS';
+import Row from '../row/Row';
+
+export default function Content() {
+  return (
+    <div className='content-wrapper'>
+
+      {RowData.map((el) => {
+        return <Row title={el.title} url={el.url} />;
+      })}
+
+      
+    </div>
+  );
+}
+
+
